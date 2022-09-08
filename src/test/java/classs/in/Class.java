75f6ac5226623dbf86e599ae743950c5be32125f
Anbukarnan <anbuvis424@gmail.com>
2022-09-08 16:38:43 +0530

    project code

--- src/test/java/classs/in/Class.java
+++ src/test/java/classs/in/Class.java
@@ -1,5 +1,11 @@
 package classs.in;
 
 public class Class {
+	public static void main(String[] args) {
+		System.out.println("code 4");
+		System.out.println("code 5");
+		System.out.println("code 6");
+		System.out.println("code 7");
+	}
 
 }
